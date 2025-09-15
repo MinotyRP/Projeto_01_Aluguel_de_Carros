61e77eb4be713ef21f1939db72ba88c194ee4600
Nikops2 <nikolasdaniel147@gmail.com>
2025-09-15 15:53:19 -0300

    novo site

--- app.py
+++ app.py
@@ -1,60 +1,59 @@
 import streamlit as st
-
+import pandas as pd
 # python -m streamlit run app.py
 
-# ------------------------------------------------- Sidebar
+#-------------------------------------- Sidebar
+
 
 st.sidebar.image("logo.png")
-st.sidebar.title('Mateus Motors')
+st.sidebar.title('Nikolas VrumVrum')
+
+carros = ['BMW', 'Mustang', 'Porsche', 'Fusca', 'Toro', 'Blaze', 'Camaro']
+
+opcao = st.sidebar.selectbox('Escolha o carroque foi alugado', carros)
 
 
-carros = ['BMW','Mustang', 'Porsche', 'Fusca', 'Toro']
 
-opcao = st.sidebar.selectbox('Escolha o carro que foi alugado', carros)
-
-
-
-# ----------------------------------------------- Principal 
-st.title('Mateus motors - Aluguel de Carros')
+#--------------------------------------- Principal
+st.title('Olá querido vendedor, vamos pra onde????🗿👌')
 
 st.image(f'{opcao}.png')
 st.markdown(f'## Você alugou o modelo: {opcao}')
 st.markdown('---')
 
-dias = st.text_input(f'Por quantos dias o {opcao} foi alugado?')
+dias = st.text_input(f'por quantos dias o {opcao} foi alugado?')
 km = st.text_input(f'Quantos km você rodou com o {opcao}?')
+
+### Copia até aqui antes do IF
 
 if opcao == 'BMW':
     diaria = 450
-
+    
 elif opcao == 'Mustang':
     diaria = 500
-
+    
 elif opcao == 'Porsche':
     diaria = 300
-
+    
 elif opcao == 'Fusca':
     diaria = 250
-
+    
 elif opcao == 'Toro':
     diaria = 550
+    
+elif opcao == 'Blaze':
+    diaria = 2.000
 
-
-
-
-
+elif opcao == 'Camaro':
+    diaria = 1.000
+    
+    
 if st.button('Calcular'):
     dias = int(dias)
     km = float(km)
-
+    
     total_dias = dias * diaria
     total_km = km * 0.15
     aluguel_total = total_dias+total_km
-
-    st.warning(f'Você alugou o {opcao} por {dias} dias e rodou {km}km. O valor total a pagar é R${aluguel_total:.2f}')
-
-
-
-
-
-
+    
+    st.warning(f'Você alugou o {opcao} por {dias} dias e rodou {km} km. Ovalor totoal a pagar é R${aluguel_total:.2f}')
